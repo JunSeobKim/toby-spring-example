70c28d5eff6e0fdf28c1292c0261ad8c06bc9c42
JunSeobKim <wnstjq616@gmail.com>
2025-02-26 00:23:36 +0900

    Add TODO

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -7,5 +7,6 @@ import java.sql.SQLException;
 public class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
+        // TODO: DB Connection
     }
 }
