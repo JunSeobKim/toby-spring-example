404192de0323e388555cf392085a7c01c764e4b9
seob1 <seob@thecoder.co.kr>
2025-02-26 00:17:24 +0900

    Initial commit

---
+++ src/springbook/user/dao/UserDao.java
@@ -0,0 +1,11 @@
+package springbook.user.dao;
+
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+    }
+}
